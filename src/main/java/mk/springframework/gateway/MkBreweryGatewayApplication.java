a932c845297114ae6b19461d3427dee45242dd52
MadanKumar <madan739@gmail.com>
2020-07-10 12:07:00 +0530

    Project initial add

---
+++ src/main/java/mk/springframework/gateway/MkBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package mk.springframework.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkBreweryGatewayApplication.class, args);
+	}
+
+}
